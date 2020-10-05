6f2e0edc05bc8a53ffc0ff028226a1ff762f0c80
leoBG <leobg@users.noreply.github.com>
2020-10-05 10:08:23 +0200

    Les nivos se finissent

---
+++ Assets/Scripts/LevelDesign/Obstacles/ScriptableTriggerFin.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ScriptTriggerFin", menuName = "Script Obstacle/TriggerFin")]
+public class ScriptableTriggerFin : ScriptableObstacle
+{
+
+}
---
+++ Assets/Scripts/LevelDesign/Obstacles/TriggerFin.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerFin : Obstacle
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpaceWheel.Instance.eventSequenceEnds.AddListener(PlayerWin);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void PlayerWin()
+    {
+        PlayerMov player;
+        if(CheckForPlayer(out player))
+        {
+            print("///// ET C'EST LE WIN!!!!! \\\\\\");
+        }
+    }
+
+    public override void LoadScriptobstacle()
+    {
+        base.LoadScriptobstacle();
+
+        ScriptableTriggerFin scriptTrigger = (ScriptableTriggerFin)scriptObstacle;
+    }
+}
