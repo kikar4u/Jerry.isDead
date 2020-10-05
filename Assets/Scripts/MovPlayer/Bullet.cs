317e78b685f5c5997c0478540db6181f02f884e5
leoBG <leobg@users.noreply.github.com>
2020-10-05 18:18:29 +0200

    Bullet

---
+++ Assets/Scripts/MovPlayer/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] private GameObject prefabSectionDefaut;
+    private float DetectionWidth
+    {
+        get
+        {
+            return 0;
+        }
+    }
+
+    private Troncon nextDirection;
+    [SerializeField] private int rangeInSection;
+    [Tooltip("secondes nécessaire pour atteindre la prochaine section")]
+    [SerializeField] private float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private Troncon SeekForNextTroncon()
+    {
+        return null;
+    }
+
+
+}
